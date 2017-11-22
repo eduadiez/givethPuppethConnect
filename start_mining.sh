30dd4028aad4f850c93bbf400e4861e7918c3ff7
Eduardo Antuña Díez <eduadiez@imagine800.es>
2017-11-22 18:37:50 +0100

    some updates

--- start_mining.sh
+++ start_mining.sh
@@ -5,8 +5,8 @@ if (( $NUM_KEYSTORE == "0" )) ; then
 echo "No etherbase set and no accounts found as default";
 docker-compose down
 docker-compose run --rm giveth_puppeth_network --datadir=/root/.giveth account new
-else
-    docker-compose up -d
-    sleep 5
-    docker-compose run --rm giveth_puppeth_network --exec "miner.start(1)" attach /root/.giveth/geth.ipc
 fi
+
+docker-compose up -d
+sleep 5
+docker-compose run --rm giveth_puppeth_network --exec "miner.start(1)" attach /root/.giveth/geth.ipc
